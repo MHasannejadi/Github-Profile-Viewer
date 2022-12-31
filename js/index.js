2fbee9a4fad3130a3ae272b8186658cb9f021a2f
MHasannejadi <hasannejadi80@gmail.com>
2022-12-31 23:59:49 +0330

    initial commit

---
+++ js/index.js
@@ -0,0 +1,59 @@
+// get data from api after form submit and set it to local storage
+async function handleSubmit(e) {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  // queries that are already fetched and stored in local storage
+  const queries = window.localStorage.getItem("queries")
+    ? JSON.parse(window.localStorage.getItem("queries"))
+    : new Map();
+  document.getElementById("message-text").innerHTML = "";
+  if (!queries[name]) {
+    // fetch data from github api
+    fetch(`https://api.github.com/users/${name}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // check if user exists
+        if (data.message) {
+          setInitialHtml();
+          document.getElementById("message-text").innerHTML = "User not found";
+        } else {
+          setHtml(data);
+          queries[name] = data;
+          window.localStorage.setItem("queries", JSON.stringify(queries));
+        }
+      });
+  } else {
+    setHtml(JSON.parse(window.localStorage.getItem("queries"))[name]);
+  }
+}
+
+// set innerHTML of elements with fetched data
+function setHtml(data) {
+  document.getElementById("username").innerHTML = data.name;
+  if (data.avatar_url) {
+    document.getElementById("img-container").innerHTML =
+      '<img src="' + data.avatar_url + '" alt="avatar" class="avatar"/>';
+  } else {
+    document.getElementById("img-container").innerHTML = "";
+  }
+  document.getElementById("blog").innerHTML = data.blog;
+  if (data.bio) {
+    document
+      .querySelector(".info-container")
+      .appendChild(document.createElement("div"));
+
+    document.getElementById("bio").innerHTML = "<pre>" + data.bio + "</pre>";
+  } else {
+    document.getElementById("bio").innerHTML = "";
+  }
+  document.getElementById("location").innerHTML = data.location;
+}
+
+// set innerHTML of elements to empty string (initial state of webpage)
+function setInitialHtml() {
+  document.getElementById("username").innerHTML = "";
+  document.getElementById("img-container").innerHTML = "";
+  document.getElementById("blog").innerHTML = "";
+  document.getElementById("bio").innerHTML = "";
+  document.getElementById("location").innerHTML = "";
+}
