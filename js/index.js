e679a4da61937b4447f0be9b119bf5e2bfd37ffd
MHasannejadi <hasannejadi80@gmail.com>
2023-01-03 23:03:08 +0330

    Fix some style issues and Add network error handling

--- js/index.js
+++ js/index.js
@@ -18,9 +18,17 @@ async function handleSubmit(e) {
           document.getElementById("message-text").innerHTML = "User not found";
         } else {
           setHtml(data);
+          // maximum cache size is 20
+          if (Object.keys(queries).length >= 20) {
+            delete queries[Object.keys(queries)[0]];
+          }
           queries[name] = data;
           window.localStorage.setItem("queries", JSON.stringify(queries));
         }
+      })
+      .catch(() => {
+        setInitialHtml();
+        document.getElementById("message-text").innerHTML = "Network Error";
       });
   } else {
     setHtml(JSON.parse(window.localStorage.getItem("queries"))[name]);
@@ -29,6 +37,7 @@ async function handleSubmit(e) {
 
 // set innerHTML of elements with fetched data
 function setHtml(data) {
+  document.getElementById("info-col").style.display = "block";
   document.getElementById("username").innerHTML = data.name;
   if (data.avatar_url) {
     document.getElementById("img-container").innerHTML =
@@ -47,6 +56,9 @@ function setHtml(data) {
     document.getElementById("bio").innerHTML = "";
   }
   document.getElementById("location").innerHTML = data.location;
+  if (data.blog === "" && !data.name && !data.location) {
+    document.getElementById("info-col").style.display = "none";
+  }
 }
 
 // set innerHTML of elements to empty string (initial state of webpage)
